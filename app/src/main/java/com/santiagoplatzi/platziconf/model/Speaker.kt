caf9eb3e84fe1e82b76a3e68bb5d65b84680a485
santiago salas <santiagosalas.6532@gmail.com>
2020-08-10 14:08:05 -0500

    Implemecion y enlace con firestore DB

---
+++ app/src/main/java/com/santiagoplatzi/platziconf/model/Speaker.kt
@@ -0,0 +1,9 @@
+package com.santiagoplatzi.platziconf.model
+
+class Speaker {
+    var name = String
+    var jobtitle = String
+    var workplace = String
+    var twitter = String
+    var image = String
+}
---
+++ app/src/main/java/com/santiagoplatzi/platziconf/model/Ubication.kt
@@ -0,0 +1,11 @@
+package com.santiagoplatzi.platziconf.model
+
+class Ubication {
+    val name = String
+    val adress = String
+    val latitude = String
+    val longitude = String
+    val phone = String
+    val website = String
+    val photo = String
+}
